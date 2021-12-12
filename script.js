582cf2fbacc71898da6cf45f6e6e5f22124dceb2
rashidul191 <rashidul.191cse.gub@gmail.com>
2021-12-12 11:07:44 +0600

    Edit space line

--- script.js
+++ script.js
@@ -2,19 +2,16 @@
 // Phone Plus
 const phonePrice = 1219;
 const phoneCount = 1;
-
 const phonePlus = document.getElementById("phonePlus");
 phonePlus.addEventListener("click", function () {
   const phoneInputCount = document.getElementById("phoneInputCount").value;
   const phoneInputCountNumber = parseFloat(phoneInputCount);
-
   const phoneTotalCount = phoneInputCountNumber + phoneCount;
+
   document.getElementById("phoneInputCount").value = phoneTotalCount;
 
   const phoneCurrentPrice = document.getElementById("phoneCurrentPrice").innerText;
-
   const phoneCurrentPriceNumber = parseFloat(phoneCurrentPrice);
-
   const totalphoneCurrentPrice = phoneCurrentPriceNumber + phonePrice;
 
   document.getElementById("phoneCurrentPrice").innerText = totalphoneCurrentPrice;
@@ -24,7 +21,6 @@ phonePlus.addEventListener("click", function () {
   const totlaPhoneSubTotal = subTotalNumber + phonePrice;
 
   document.getElementById("subTotal").innerText = totlaPhoneSubTotal;
-
   document.getElementById("totalBalance").innerText = totlaPhoneSubTotal;
 });
 
@@ -36,27 +32,23 @@ phoneMinus.addEventListener("click", function () {
 
   if (phoneInputCountNumber != 1) {
     const phoneTotalCount = phoneInputCountNumber - phoneCount;
+
     document.getElementById("phoneInputCount").value = phoneTotalCount;
 
     const phoneCurrentPrice = document.getElementById("phoneCurrentPrice").innerText;
-
     const phoneCurrentPriceNumber = parseFloat(phoneCurrentPrice);
-
     const totalphoneCurrentPrice = phoneCurrentPriceNumber - phonePrice;
 
     document.getElementById("phoneCurrentPrice").innerText = totalphoneCurrentPrice;
 
     const subTotal = document.getElementById("subTotal").innerText;
-
     const subTotalNumber = parseFloat(subTotal);
     const totlaPhoneSubTotal = subTotalNumber - phonePrice;
 
     document.getElementById("subTotal").innerText = totlaPhoneSubTotal;
-
     document.getElementById("totalBalance").innerText = totlaPhoneSubTotal;
   } else {
     document.getElementById("phoneInputCount").value = phoneCount;
-
     document.getElementById("phoneCurrentPrice").innerText = phonePrice;
   }
 });
@@ -65,17 +57,15 @@ phoneMinus.addEventListener("click", function () {
 // Phone Case Plus
 const phoneCasePrice = 59;
 const phoneCaseCournt = 1;
-
 const phoneCasePlus = document.getElementById("phoneCasePlus");
 phoneCasePlus.addEventListener("click", function () {
   const phoneCaseInputCount = document.getElementById("phoneCaseInputCount").value;
   const phoneCaseCountNumber = parseFloat(phoneCaseInputCount);
-
   const totalPhoneCaseCountNumber = phoneCaseCountNumber + phoneCaseCournt;
+
   document.getElementById("phoneCaseInputCount").value = totalPhoneCaseCountNumber;
 
   const phoneCaseCurrentPrice = document.getElementById("phoneCaseCurrentPrice").innerText;
-
   const phoneCaseCurrentPriceNumber = parseFloat(phoneCaseCurrentPrice);
   const totalPhoneCaseCurrentPriceNumber = phoneCaseCurrentPriceNumber + phoneCasePrice;
 
@@ -99,20 +89,19 @@ phoneCaseMinus.addEventListener("click", function () {
     const totalPhoneCaseCountNumber = phoneCaseCountNumber - phoneCaseCournt;
     document.getElementById("phoneCaseInputCount").value = totalPhoneCaseCountNumber;
     const phoneCaseCurrentPrice = document.getElementById("phoneCaseCurrentPrice").innerText;
-
     const phoneCaseCurrentPriceNumber = parseFloat(phoneCaseCurrentPrice);
     const totalPhoneCaseCurrentPriceNumber = phoneCaseCurrentPriceNumber - phoneCasePrice;
 
     document.getElementById("phoneCaseCurrentPrice").innerText = totalPhoneCaseCurrentPriceNumber;
-    const subTotal = document.getElementById("subTotal").innerText;
 
+    const subTotal = document.getElementById("subTotal").innerText;
     const subTotalNumber = parseFloat(subTotal);
     const totlaCaseSubTotal = subTotalNumber - phoneCasePrice;
+
     document.getElementById("subTotal").innerText = totlaCaseSubTotal;
     document.getElementById("totalBalance").innerText = totlaCaseSubTotal;
   } else {
     document.getElementById("phoneCaseInputCount").value = phoneCaseCournt;
-
     document.getElementById("phoneCaseCurrentPrice").innerText = phoneCasePrice;
   }
 });
@@ -122,14 +111,11 @@ phoneCaseMinus.addEventListener("click", function () {
 function removePhoneIteam() {
   const phoneCurrentPrice = document.querySelector("#phoneCurrentPrice").innerText;
   const phoneCurrentPriceNuber = parseFloat(phoneCurrentPrice);
-  // console.log(phoneCurrentPriceNuber)
-
   const removePhoneSubTotal = document.querySelector("#subTotal").innerText;
   const removePhoneSubTotalNumber = parseFloat(removePhoneSubTotal);
   const subTotal = removePhoneSubTotalNumber - phoneCurrentPriceNuber;
 
   document.querySelector("#subTotal").innerText = subTotal;
-
   document.querySelector("#totalBalance").innerText = subTotal;
   document.querySelector("#phoneItem").style.display = "none";
 }
@@ -139,14 +125,11 @@ function removePhoneIteam() {
 function caseRemoveIteam() {
   const phoneCaseCurrentPrice = document.querySelector("#phoneCaseCurrentPrice").innerText;
   const phoneCaseCurrentPriceNumber = parseFloat(phoneCaseCurrentPrice);
-
-
-
   const currentSubTotal = document.querySelector("#subTotal").innerText;
   const currentSubTotalNumber = parseFloat(currentSubTotal);
+
   const subTotal = currentSubTotalNumber - phoneCaseCurrentPriceNumber;
   document.querySelector("#subTotal").innerText = subTotal;
-
   document.querySelector("#totalBalance").innerText = subTotal;
   document.querySelector("#phoneCaseIteam").style.display = "none";
 }
